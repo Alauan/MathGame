afa6667ee26024fee9231c02eac803307de4f512
Alauan <67768915+Alauan@users.noreply.github.com>
2020-12-23 19:44:49 -0400

    -> Added Angle class with support for "square" 90º angles is any position -> Added function to tell if polygon is in the right spot

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from pygame.locals import *
 import sys
+from math import sin, cos
 from typing import List
 
 clock = pg.time.Clock()
@@ -12,7 +13,7 @@ TRANSPARENT_RED = pg.Color(255, 50, 50, 160)
 TRANSPARENT_GREEN = pg.Color(10, 220, 10, 160)
 TRANSPARENT_YELLOW = pg.Color(255, 255, 160, 160)
 RED = pg.Color(255, 40, 40)
-GREEN = pg.Color(10, 220, 10)
+GREEN = pg.Color(10, 190, 10)
 YELLOW = pg.Color(255, 255, 0)
 BLACK = pg.Color(0, 0, 0)
 TRANSPARENT = (0, 0, 0, 0)
@@ -51,14 +52,14 @@ for index, linha in enumerate(Enunciado):
     levelSurf.blit(font_enunciado.render(linha, True, BLACK), (60, 25*index+40))
 
 # variables
-mouse = {'buttons': [False, False, False], 'pos': (0, 0), 'just_pressed': [False, False, False]}
-ini_mouse = mouse['pos']
+mouse = {'buttons': [False, False, False], 'pos_rect': (0, 0), 'just_pressed': [False, False, False]}
+ini_mouse = mouse['pos_rect']
 user_input = ''
 
 
 # classes
 class Polygon:
-    def __init__(self, window_size, points, right_point=(0, 0), color=(255, 50, 50, 140)):
+    def __init__(self, window_size, points, right_point=(0, 0), color=RED):
         self.surface = pg.Surface(window_size, flags=SRCALPHA)
         self.ini_points = points
         self.points = points
@@ -78,8 +79,7 @@ class Polygon:
                 self.is_hold = False
                 if abs(self.points[0][0] - self.right_point[0]) < 15 and \
                         abs(self.points[0][1] - self.right_point[1]) < 15:
-                    self.move((0, 0), (self.right_point[0] - self.points[0][0],
-                                       self.right_point[1] - self.points[0][1]))
+                    self.move(self.points[0], self.right_point)
             else:
                 self.move(ini_cursor, cursor)
 
@@ -102,10 +102,13 @@ class Polygon:
         self.points = [[self.points[p][0] + fin_cursor[0] - ini_cursor[0],
                         self.points[p][1] + fin_cursor[1] - ini_cursor[1]] for p in range(self.n_sides)]
 
+    def is_in_right_point(self):
+        return self.points[0][0] == self.right_point[0] and self.points[0][1] == self.right_point[1]
+
     def draw(self):
         """ Draws the polygon onto its surface """
+        pg.draw.polygon(self.surface, (0, 0, 0, 30), self.points)
         pg.draw.aalines(self.surface, self.color, True, self.points)
-        pg.draw.polygon(self.surface, self.color, self.points)
 
 
 class Click:
@@ -165,12 +168,52 @@ class Click:
         self.surface.blit(self.symbol_surf, self.pos)
 
 
-polygons = [Polygon(WINDOWSIZE, [[640, 430], [640, 580], [940, 580], [940, 430]], (170, 200)),
-            Polygon(WINDOWSIZE, [[640, 430], [900, 430], [640, 580]], (471, 200), TRANSPARENT_GREEN),
-            Polygon(WINDOWSIZE, [[600, 430], [600, 580], [515, 430]], (169, 200), TRANSPARENT_YELLOW)]
+class Angle:
+    def __init__(self, comeco, grau, pos, surface, num_pos=None, tamanho=50):
+        self.comeco_rad = comeco / 57.29
+        self.grau_rad = grau / 57.29
+        self.grau = grau
+        self.pos_rect = (pos[0] - tamanho // 2, pos[1] - tamanho // 2)
+        self.pos = pos
+        self.surface = surface
+        self.num_pos = num_pos
+        self.tamanho = tamanho
 
-clicaveis = [Click('α', (331, 211), clickSurf, '90', [57, 62, 253, 23]),
+        if num_pos:
+            font = pg.font.SysFont('Calibri', 20)
+            self.num_surf = font.render(str(grau) + '°', True, RED)
+        else:
+            self.num_surf = None
+
+    def draw(self):
+        if self.grau != 90:
+            pg.draw.arc(self.surface, RED, (self.pos_rect[0], self.pos_rect[1], self.tamanho, self.tamanho),
+                        self.comeco_rad, self.comeco_rad + self.grau_rad, 2)
+        else:
+            pg.draw.aalines(self.surface, RED, False,
+                            [(self.pos[0] + self.tamanho // 2 * cos(self.comeco_rad),
+                              self.pos[1] + -self.tamanho // 2 * sin(self.comeco_rad)),
+                             (self.pos[0] + self.tamanho * 1.41 // 2 * cos(self.comeco_rad + 0.78),
+                              self.pos[1] + -self.tamanho * 1.41 // 2 * sin(self.comeco_rad + 0.78)),
+                             (self.pos[0] + self.tamanho // 2 * cos(self.comeco_rad + 1.57),
+                              self.pos[1] + -self.tamanho // 2 * sin(self.comeco_rad + 1.57))])
+            pg.draw.circle(self.surface, RED,
+                           (self.pos[0] + self.tamanho * 0.7 // 2 * cos(self.comeco_rad + 0.78),
+                            self.pos[1] + -self.tamanho * 0.7 // 2 * sin(self.comeco_rad + 0.78)), 2, 3)
+        if self.num_surf:
+            self.surface.blit(self.num_surf, self.num_pos)
+
+
+polygons = [Polygon(WINDOWSIZE, [[640, 430], [640, 580], [940, 580], [940, 430]], (170, 200)),
+            Polygon(WINDOWSIZE, [[640, 430], [900, 430], [640, 580]], (471, 200), GREEN),
+            Polygon(WINDOWSIZE, [[600, 430], [600, 580], [515, 430]], (169, 200), YELLOW)]
+
+clicaveis = [Click('α', (346, 218), clickSurf, '90', [57, 62, 253, 23]),
              Click('R', (340, 285), clickSurf, '9', [595, 37, 96, 23])]
+
+angulos = [[Angle(270, 90, (320, 200), clickSurf)],
+           [Angle(0, 120, (170, 350), clickSurf, (195, 310))],
+           [Angle(30, 150, (470, 350), clickSurf, (398, 307))]]
 
 # main loop
 while True:
@@ -185,12 +228,12 @@ while True:
             if event.button <= 3:
                 mouse['buttons'][event.button - 1] = True
                 mouse['just_pressed'][event.button - 1] = True
-                print(mouse['pos'])
+                print(mouse['pos_rect'])
         elif event.type == MOUSEBUTTONUP:
             if event.button <= 3:
                 mouse['buttons'][event.button - 1] = False
         elif event.type == MOUSEMOTION:
-            mouse['pos'] = event.pos
+            mouse['pos_rect'] = event.pos
 
         if event.type == KEYDOWN:
             if event.key == K_BACKSPACE:
@@ -202,20 +245,32 @@ while True:
 
     # processamento
     for poly in polygons:
-        poly.update(mouse['just_pressed'][0], mouse['pos'], ini_mouse, mouse['buttons'][0])
+        poly.update(mouse['just_pressed'][0], mouse['pos_rect'], ini_mouse, mouse['buttons'][0])
     for clicavel in clicaveis:
-        clicavel.update(mouse['just_pressed'][0], mouse['pos'], user_input)
+        clicavel.update(mouse['just_pressed'][0], mouse['pos_rect'], user_input)
+    for conjunto in angulos:
+        for angulo in conjunto:
+            angulo.draw()
 
-    for poly in polygons:
-        screen.blit(poly.surface, (0, 0))
+    if polygons[0].is_in_right_point():
+        if len(angulos[1]) == 1:
+            angulos[1] = [Angle(0, 90, (170, 350), clickSurf, (195, 310)),
+                          Angle(90, 30, (170, 350), clickSurf, (142, 284), tamanho=70)]
+        if len(angulos[2]) == 1:
+            angulos[2] = [Angle(30, 60, (470, 350), clickSurf, (480, 301)),
+                          Angle(90, 90, (470, 350), clickSurf, (412, 310))]
+
+    # screen assembly
     screen.blit(clickSurf, (0, 0))
     screen.blit(levelSurf, (0, 0))
+    for poly in polygons:
+        screen.blit(poly.surface, (0, 0))
 
     # screen update
     clock.tick(60)
     pg.display.update()
 
     # variable updates
-    ini_mouse = mouse['pos']
+    ini_mouse = mouse['pos_rect']
     user_input = ''
     mouse['just_pressed'] = [False, False, False]
